de5461bf2670f68bd388b3ffe38d767f397d4863
fjovanovic <filipjovanovic98@gmail.com>
2024-11-07 23:29:39 +0100

    Updated query params

--- routers/txt2img/txt2img.py
+++ routers/txt2img/txt2img.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Query
-from fastapi.responses import JSONResponse
 
 
 router = APIRouter(
@@ -18,17 +17,43 @@ async def generate_images(
     prompt: str = Query(
         description='The prompt or prompts to guide image generation'
     ),
-    height: int = Query(
-        default=512, 
-        enum=[512, 1024], 
-        description='The height in pixels of the generated image'
+    negative_prompt: str = Query(
+        default='',
+        description='The prompt or prompts to guide what to not include in image generation'
+    ),
+    scheduler: str = Query(
+        default='DPM-Solver',
+        enum=[
+            'DDIM',
+            'PLMS',
+            'Euler',
+            'LMS',
+            'DPM-Solver'
+        ],
+        description='A scheduler to be used in combination with unet to denoise the encoded image latents'
+    ),
+    num_of_images: int = Query(
+        default=4,
+        ge=1,
+        le=4,
+        description='The number of images to generate per prompt'
+    ),
+    seed: int = Query(
+        description='Number to make generation deterministic'
     ),
     width: int = Query(
         default=512, 
-        enum=[512, 1024], 
+        ge=512,
+        le=1024,
         description='The width in pixels of the generated image'
     ),
-    num_inference_steps: int = Query(
+    height: int = Query(
+        default=512, 
+        ge=512,
+        le=1024,
+        description='The height in pixels of the generated image'
+    ),
+    inference_steps: int = Query(
         default=50,
         ge=10,
         le=50,
@@ -36,7 +61,7 @@ async def generate_images(
             'More denoising steps usually lead to a higher ' \
             'quality image at the expense of slower inference'
     ),
-    guidance_scale: int = Query(
+    cfg_scale: float = Query(
         default=7.5,
         ge=1,
         le=10,
@@ -44,17 +69,6 @@ async def generate_images(
             'the model to generate images closely linked ' \
             'to the text prompt at the expense of lower image quality. ' \
             'Guidance scale is enabled when **guidance_scale > 1**'
-    ),
-    negative_prompt: str = Query(
-        default='', 
-        description='The prompt or prompts to guide what to ' \
-            'not include in image generation. If not defined, ' \
-            'you need to pass negative_prompt_embeds instead'
-    ),
-    num_images_per_prompt: str = Query(
-        default=1,
-        description='The number of images to generate per prompt'
     )
-) -> str:
-    
-    return JSONResponse(content={'message': 'Hello'})
+):
+    ...
